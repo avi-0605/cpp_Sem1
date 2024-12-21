25578fc78a0d9b4eb0973e862ecb69ff07032219
avi-0605 <aavanirp@gmail.com>
2024-12-21 16:17:49 +0530

    Add files via upload

---
+++ Assignment_2/assignment-2.cpp
@@ -0,0 +1,178 @@
+
+//#include<iostream>
+// swap two numbers by using 3 rd variable
+// int main(){
+//     int x=10;
+//     int y=20;
+//     int temp=x;
+//     x=y;
+//     y=temp;
+//     std::cout<<x<<std::endl;
+//     std::cout<<y<<std::endl;
+// }
+
+
+// ------------------------------
+
+// #include<iostream>
+// user input values 
+// using namespace std;
+// int main() {
+//     int a,b;
+//     cout<<"Enter a num ";
+//     cin>>a;
+//     cout<<"Enter b num ";
+//     cin>>b;
+//     cout<<"value of a "<<a<<endl;
+//     cout<<"value of b "<<b<<endl;
+//     return 0;
+// }
+
+
+// ------------------------------
+
+//#include<iostream>
+// simple arithmetic program 
+// using namespace std;
+// int main() {
+//     int a=10;
+//     int b=20;
+//     int c=a+b;
+//     int d=a-b;
+//     int e=a*b;
+//     int f=a/b;
+//     cout<<"addition "<<c<<endl;
+//     cout<<"subtraction "<<d<<endl;
+//     cout<<"multiplication "<<e<<endl;
+//     cout<<"division "<<f<<endl;
+//     return 0;
+//     }
+
+
+//---------------------------------
+
+// #include<iostream>
+// // find the sum of multiple variable 
+// int main() {
+//     int num1, num2,num3,sum;
+
+//     std::cout << "Enter first number: ";
+//     std::cin >> num1;
+//     std::cout << "Enter second number: ";
+//     std::cin >> num2;
+//     std::cout<<"Enter third number:";
+//     std::cin>>num3 ;
+
+//     sum = num1 + num2 + num3;
+
+//     std::cout << "Sum: " << sum << std::endl;
+//     return 0;
+// }
+
+
+//--------------------------------------------------
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main() {
+//     int integerValue = 42;
+//     float floatValue = 3.14f;
+//     string stringValue = "Hello, World!";
+//     cout << "Integer: " << integerValue << endl;
+//     cout << "Float: " << floatValue << endl;
+//     cout << "String: " << stringValue << endl;
+//     return 0;
+// }
+
+//----------------------------------------------------------
+
+// #include <iostream>
+
+// int main() {
+//     float fahrenheit, celsius;
+//     std::cout << "Enter temperature in Fahrenheit: ";
+//     std::cin >> fahrenheit;
+//     celsius = (5.0 / 9.0) * (fahrenheit - 32);
+//     std::cout << fahrenheit << " degrees Fahrenheit is equal to " << celsius << " degrees Celsius." << std::endl;
+//     return 0;
+// }
+
+// ---------------------------------------------------------
+// to store and print string value 
+// #include<iostream>
+// #include <string>
+// using namespace std;
+// int main() {
+//     string stringValue = "Hello, World!";
+//         cout << "String: " << stringValue << endl;
+// }
+
+// ---------------------------------------------------------
+//Simple interest 
+
+// #include <iostream>
+// using namespace std;
+
+// float SI(int P, int T, float R) {
+//     float SI;
+//     SI = (P * T * R) / 100;
+//     return SI;
+// }
+
+// int main() {
+//     int P = 10000; // Principal amount
+//     int T = 7;     // Time in years
+//     float R = 9; // Rate of interest
+
+//     cout << "SIMPLE INTEREST"<< SI (P, T, R) << endl;
+//     return 0;
+//     }
+
+// ---------------------------------------------------------
+
+
+// #include<iostream>
+// // simple division 
+// using namespace std;
+// int main() {
+//     int a=100;
+//     int b=20;
+//     int f=a/b;
+//     cout<<"division "<<f<<endl;
+//     return 0;
+//     }
+
+//--------------------------------------------------------------
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int length, width, perimeter;
+//     cout << "Enter the length of the rectangle: ";
+//     cin >> length;
+//     cout << "Enter the width of the rectangle: ";
+//     cin >> width;
+//     perimeter = 2 * (length + width);
+//     cout << "The perimeter of the rectangle is: " << perimeter << endl;
+//     return 0;
+// }
+
+//---------------------------------------------------------------------
+
+//negative positive
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter the value of a :";
+//     cin>>a;
+//     if (a>0)
+//     {
+//         cout<<"The value of a is positive"<<endl;
+//     }
+//     else if (a<0)
+//     {
+//         cout<<"The value of a is negative"<<endl;
+//       return 0;  
+//  }
