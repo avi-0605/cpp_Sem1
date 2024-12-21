f9b933c6ca5c33eb476d402c3da9df9c2a7e7c1d
avi-0605 <aavanirp@gmail.com>
2024-12-21 16:18:56 +0530

    Add files via upload

---
+++ Assignment_3/assignment3.cpp
@@ -0,0 +1,153 @@
+//  simple program to check if a number is positive, negative, or zero
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter the value of a :";
+//     cin>>a;
+//     if (a>0)
+//     {
+//         cout<<"The value of a is positive"<<endl;
+//     }
+//     else if (a<0)
+//     {
+//         cout<<"The value of a is negative"<<endl; 
+//     }
+//     else
+//     {
+//         cout<<"The value of a is zero"<<endl;
+//     }
+//     return 0;
+// }
+
+
+//-------------------------------------------------------------------
+
+
+//A program that checks if a number is even or odd.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter the value of a :";
+//     cin>>a;
+//     if (a%2==0)
+//     {
+//         cout<<"The value of a is even"<<endl;
+//     }
+//     else
+//     {
+//         cout<<"The value of a is odd"<<endl;
+//     }
+//         return 0;
+// }
+
+
+// a program that displays the name of the day based on the number entered by the user    (1 for Monday, 2 for Tuesday, etc.).
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter the value of a:";
+//     cin>>a;
+// switch (a){
+//     case 1:
+//         a==1;
+//         cout<<"Monday"<<endl;
+//     break;
+//     case 2:
+//         a==2;
+//         cout<<"Tuesday"<<endl;
+//     break;
+//     case 3:
+//         a==3;
+//         cout<<"Wednesday"<<endl;
+//     break;
+//     case 4:
+//         a==4;
+//         cout<<"Thursday"<<endl;
+//     break;
+//     case 5:
+//           a==5;
+//           cout<<"Friday"<<endl;
+//     break;
+//     case 6:
+//           a==6;
+//           cout<<"Saturday"<<endl;
+//     break ;
+
+
+// }
+
+//A simple grading system using switch to select the grade based on marks.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter the marks of the student:";
+//     cin>>a;
+//     if (a>80)
+//     {
+//         cout<<"Grade A"<<endl;
+//     }
+//     else if (a>60)
+//     {
+//         cout<<"Grade B"<<endl; 
+//     }
+//     else
+//     {
+//         cout<<"Grade C "<<endl;
+//     }
+//     return 0;
+// }
+
+//-----------------------------------------------------------------
+
+ //A program that uses if and else statements to find the largest of three numbers.
+//  #include<iostream>
+//  using namespace std;
+// int main(){
+//     int a ,b,c;
+//     cout<<"Enter the first number:";
+//     cin>>a;
+//     cout<<"Enter the second number:";
+//     cin>>b;
+//     cout<<"Enter the third number;";
+//     cin>>c;
+//     if (a>b && a>c)
+//     {
+//         cout<<"The largest number is "<<a<<endl;
+//     }
+//     else if (b>a && b>c)
+//     {
+//         cout<<"The largest number is "<<b<<endl;
+//     }
+//     else
+//     {
+//         cout<<"The largest number is "<<c<<endl;
+//     }
+//     return 0;
+// }
+
+
+//7.A program to check whether a given year is a leap year.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int y;
+//     cout<<"Enter the year:";
+//     cin>>y;
+//     if (y%4==0 and y%100==0)
+//     {
+//         cout<<"TIt is a leap year"<<y<<endl;
+//     }
+//     else
+//     {
+//         cout<<"It is not a leap year"<<endl;
+//     }
+//     return 0;
+//     }  
+
+
+
+
